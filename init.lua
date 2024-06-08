fe15553f6aebf04618e4454a8f60d6cd989d5e51
almelo <akaalga@proton.me>
2024-06-08 12:41:25 -0300

    feat: disable tab handler plugin (sleuth), to control it manually

--- init.lua
+++ init.lua
@@ -90,6 +90,15 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- See :help tabstop
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Disable swap file
+vim.opt.swapfile = false
+
+vim.opt.colorcolumn = '80'
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
@@ -226,7 +235,8 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -623,43 +633,6 @@ require('lazy').setup({
         },
       }
     end,
-  },
-
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    lazy = false,
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      --format_on_save = function(bufnr)
-      --  -- Disable "format_on_save lsp_fallback" for languages that don't
-      --  -- have a well standardized coding style. You can add additional
-      --  -- languages here or re-enable it for the disabled ones.
-      --  local disable_filetypes = { c = true, cpp = true }
-      --  return {
-      --    timeout_ms = 500,
-      --    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      --  }
-      --end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
-    },
   },
 
   { -- Autocompletion
